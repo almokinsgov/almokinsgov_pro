1dd20ea7f85659c5b0641dfec69298cc1e96d753
Amorangi Mathews <olafbig@hotmail.com>
2026-09-02 00:22:16 +1200

    Add files via upload

---
+++ jukebox/0_5_1/static-config.js
@@ -0,0 +1,18 @@
+window.JUKEBOX_STATIC_CONFIG = {
+  "version": "0.5.1-static",
+  "spotifyClientId": "fdc4249f1fd24555a5aaef131c4c4cec",
+  "spotifyRedirectUri": "https://almokinsgov.dev/jukebox/0_5_1/",
+  "mockMode": false,
+  "peerPrefix": "gnj-v051-",
+  "peerClientUrl": "https://cdn.jsdelivr.net/npm/peerjs@1.5.5/dist/peerjs.min.js",
+  "peer": {
+    "host": "",
+    "port": 443,
+    "path": "/",
+    "secure": true,
+    "key": "",
+    "debug": 0,
+    "iceServers": []
+  },
+  "qrEndpoint": "https://quickchart.io/qr"
+};
