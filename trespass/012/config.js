6b585d20ed2588714f2657a85ae0c7f738285f83
Amorangi Mathews <olafbig@hotmail.com>
2026-09-17 14:19:06 +1200

    Update config.js

--- trespass/012/config.js
+++ trespass/012/config.js
@@ -1,8 +1,8 @@
 // Office Traffic Light v0.1.3
 // Replace these values before deployment.
 window.OFFICE_SAFETY_CONFIG = {
-  apiUrl: "PASTE_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  googleClientId: "PASTE_GOOGLE_OAUTH_WEB_CLIENT_ID_HERE",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwcSVXDVzNUQp41MOFu5CW6smUa8oc7iqsHIlvrp9_lzZOZYe4A0YsmEMjG6zvMIxvh/exec",
+  googleClientId: "445205554964-8mvgjbdnkc5e956ri2r4i4oe5hd78bhs.apps.googleusercontent.com",
   appName: "Office Safety",
   allowedDomainHint: "",
   defaultPollMs: 10000,
