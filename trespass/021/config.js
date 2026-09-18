473d03c5dd145e09a302849c2e8aff7bb44020ac
Amorangi Mathews <olafbig@hotmail.com>
2026-09-18 12:16:21 +1200

    Update API URL in config.js

--- trespass/021/config.js
+++ trespass/021/config.js
@@ -1,7 +1,7 @@
 // Office Traffic Light v0.2.1
 // Replace these values before deployment.
 window.OFFICE_SAFETY_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/AKfycbwppQ51-w6clXoIvZULr7ZIoNp7WivLtvk-22daNoSJBom5VQoUiES9eH4yXpUNtfBi/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbxTyK_ahZan3kyELr6IjB7mTbYwGPaUFMAwUQnxGoDn6iMa3Ewdw_9nasiel4KBcf8j/exec",
   googleClientId: "445205554964-8mvgjbdnkc5e956ri2r4i4oe5hd78bhs.apps.googleusercontent.com",
   appName: "Office Safety",
   allowedDomainHint: "", // Optional Workspace-only account picker hint. Leave blank to allow consumer Google accounts such as Gmail.
